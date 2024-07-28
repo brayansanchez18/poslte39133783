d15c2cdcfd563baa4778330b3434a58b872be41a
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2024-07-27 20:13:45 -0600

    creacion de modulos del proyecto

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 4.7.2
+  </div>
+  <strong><a href="#">NEMStudios &copy;</a> </strong> All rights reserved.
+</footer>
---
+++ vistas/modulos/lateral.php
@@ -0,0 +1,97 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="../../index3.html" class="brand-link">
+    <img src="vistas/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">POS39133783</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="vistas/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+      </div>
+      <div class="info">
+        <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="" class="btn btn-block btn-danger btn-sm mt-1">Salir</a>
+      </div>
+    </div>
+
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+        <li class="nav-item">
+          <a href="../widgets.html" class="nav-link">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Inicio</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="../widgets.html" class="nav-link">
+            <i class="nav-icon fas fa-user-cog"></i>
+            <p>Usuarios</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="../widgets.html" class="nav-link">
+            <i class="nav-icon fas fa-boxes"></i>
+            <p>Categorias</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="../widgets.html" class="nav-link">
+            <i class="nav-icon fas fa-box"></i>
+            <p>Productos</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="../widgets.html" class="nav-link">
+            <i class="nav-icon fas fa-user-tag"></i>
+            <p>Clientes</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-shopping-cart"></i>
+            <p>
+              Ventas
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="../../index.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Administrar Ventas</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="../../index2.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Crear Ventas</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a href="../widgets.html" class="nav-link">
+            <i class="nav-icon fas fa-chart-line"></i>
+            <p>Reportes de Ventas</p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
